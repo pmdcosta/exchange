80c4c3ffd60042119e406ba42a6b0d5191789b2d
Pedro Costa <pedro.dacosta@perkbox.com>
2019-11-18 04:33:31 +0000

    implemented recommendation engine

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,30 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/pmdcosta/exchange/internal/handler"
+	"github.com/pmdcosta/exchange/internal/recommendation"
 	"github.com/pmdcosta/exchange/pkg/exchange"
 	"github.com/pmdcosta/exchange/pkg/memory"
+	"github.com/pmdcosta/exchange/pkg/server"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"golang.org/x/text/currency"
 )
 
 func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
 	// initialize clients
 	l := log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
 	cache := memory.New(&l)
 	client := exchange.NewClient(&l, exchange.SetCache(cache))
-
-	// make request
-	data, err := client.WithOptions(
-		exchange.SetBaseCurrency(currency.EUR),
-		exchange.SetCurrencies(currency.USD, currency.GBP),
-	).GetLatest()
-	if err != nil {
-		l.Fatal().Err(err).Msg("failed to get latest rates")
-	}
-	fmt.Println(err, data)
+	s := server.New(&l, "localhost:8080")
+	h := handler.New(&l, client, recommendation.Naive)
+	go s.Start(h.Routes)
+	defer s.Done()
+	<-sigs
 }
